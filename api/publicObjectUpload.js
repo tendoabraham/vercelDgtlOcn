504b91f73cdcd60ad0b2a3c4bbb8301062de6288
Tendo Abraham <tendoabraham@gmail.com>
2024-11-25 15:04:01 +0300

    uploadPublicFile

---
+++ api/publicObjectUpload.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  endpoint: 'https://ams3.digitaloceanspaces.com', // DigitalOcean Spaces endpoint
+  accessKeyId: process.env.SPACES_KEY,
+  secretAccessKey: process.env.SPACES_SECRET,
+});
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { fileName, fileType } = req.body;
+
+    const params = {
+      Bucket: 'thetribeug',
+      Key: fileName,
+      Expires: 3600, // Link valid for 60 seconds
+      ContentType: fileType,
+      ACL: 'public-read', // Make the object publicly readable
+    };
+
+    try {
+      const url = s3.getSignedUrl('putObject', params);
+      res.status(200).json({ url });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
