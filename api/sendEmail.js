a8b5ff8f64a763077b719c246361ae197cb760e0
Tendo Abraham <tendoabraham@gmail.com>
2025-04-12 11:22:15 +0300

    creds

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -19,17 +19,29 @@ export default async function handler(req, res) {
   const { name, email, message } = req.body;
 
   // Create a transporter using Gmail
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USER, // your Gmail
-      pass: process.env.EMAIL_PASS, // App Password (not Gmail password)
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//       user: process.env.EMAIL_USER, // your Gmail
+//       pass: process.env.EMAIL_PASS, // App Password (not Gmail password)
+//     },
+//   });
+
 
   try {
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT),
+        secure: true,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      });
+
     await transporter.sendMail({
-      from: `"Creators" <${process.env.EMAIL_USER}>`,
+      from: `"Creators" <${process.env.SMTP_USER}>`,
       to: 'appsofimpact@gmail.com',
       subject: 'Creator Content Submission',
       html: `
